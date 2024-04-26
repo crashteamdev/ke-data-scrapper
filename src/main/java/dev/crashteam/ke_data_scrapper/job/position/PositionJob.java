3a9694ee1540c6694b0f9e27b776b018e166769e
SageFreke <lenigrast@gmail.com>
2024-04-26 18:49:01 +0300

    increase position work pool

--- src/main/java/dev/crashteam/ke_data_scrapper/job/position/PositionJob.java
+++ src/main/java/dev/crashteam/ke_data_scrapper/job/position/PositionJob.java
@@ -58,7 +58,7 @@ public class PositionJob implements Job {
     @Value("${app.stream.position.waitPending}")
     public Long waitPending;
 
-    ExecutorService jobExecutor = Executors.newWorkStealingPool(3);
+    ExecutorService jobExecutor = Executors.newWorkStealingPool(6);
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
