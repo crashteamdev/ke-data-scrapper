755f862d0c20bf6c9ae0f93d8946cd8031aa1787
SageFreke <lenigrast@gmail.com>
2024-05-05 12:26:56 +0300

    add more user agents

--- src/main/java/dev/crashteam/ke_data_scrapper/util/RandomUserAgent.java
+++ src/main/java/dev/crashteam/ke_data_scrapper/util/RandomUserAgent.java
@@ -23,7 +23,17 @@ public class RandomUserAgent {
                 "Netty/2.4.1",
                 "RandUse/4.44",
                 "SomeOther/5.35",
-                "Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+                "Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+                "Mozilla/5.0 (Windows; U; Windows NT 6.1; ar; rv:1.9.2) Gecko/20100115 Firefox/3.6",
+                "Mozilla/5.0 (Windows; U; Windows NT 6.0; ru; rv:1.9.2) Gecko/20100115 Firefox/3.6",
+                "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1b5pre) Gecko/20090517 Firefox/3.5b4pre (.NET CLR 3.5.30729)",
+                "Mozilla/5.0 (X11; U; Gentoo Linux x86_64; pl-PL) Gecko Firefox",
+                "Mozilla/5.0 (X11; ; Linux x86_64; rv:1.8.1.6) Gecko/20070802 Firefox",
+                "Opera/9.80 (S60; SymbOS; Opera Tablet/9174; U; en) Presto/2.7.81 Version/10.5",
+                "Opera/9.80 (X11; U; Linux i686; en-US; rv:1.9.2.3) Presto/2.2.15 Version/10.10",
+                "Opera/9.80 (X11; Linux x86_64; U; it) Presto/2.2.15 Version/10.10",
+                "Opera/9.80 (Windows NT 6.1; U; de) Presto/2.2.15 Version/10.10",
+                "Opera/9.80 (Windows NT 6.0; U; Gecko/20100115; pl) Presto/2.2.15 Version/10.10",
         });
     }
 
