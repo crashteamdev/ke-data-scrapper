d96d84fb70ab65cbe95063b1932d46c90874c481
SageFreke <lenigrast@gmail.com>
2024-05-14 11:28:10 +0300

    fixed cache

--- src/main/java/dev/crashteam/ke_data_scrapper/configuration/RedisConfiguration.java
+++ src/main/java/dev/crashteam/ke_data_scrapper/configuration/RedisConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceClientConfigurat
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -64,6 +65,8 @@ public class RedisConfiguration {
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
         return builder -> builder
                 .withCacheConfiguration("productCache",
-                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(24)));
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                                .entryTtl(Duration.ofHours(24)));
     }
 }
